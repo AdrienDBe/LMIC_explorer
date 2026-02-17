bb58a343d776af9accba4b20845a073c64b5c73c
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2026-02-17 14:02:29 +0100

    Add radio button for map display type selection

--- Home.py
+++ Home.py
@@ -840,6 +840,7 @@ else:
             "Display Type:",
             options=["Publications", "Authors", "Organizations"],
             index=1,
+            display_type = search_col1.radio,
             key="map_display_type_radio"
         )
     
